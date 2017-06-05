a9db5e7007d06b71a6a7192b24d70672693253b9
Adrià Fontcuberta <adria@calidae.com>
2017-06-05 15:37:50 +0200

    replace class component with functional component

--- src/components/Idea/Idea.jsx
+++ src/components/Idea/Idea.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import './Idea.css';
 
-class Idea extends React.Component {
+function Idea(props) {
+  return (
+    <li
+      className={"Idea " + (props.isUpvoted ? 'Idea--upvoted' : '')}
+      onClick={props.onClick}>
+      <p className="Idea__summary">{props.summary} (by {props.userName})</p>
+      <div className="Idea__upvotes">{props.upvotes}</div>
+    </li>
+  );
 
-  render() {
-    return (
-      <li
-        className={"Idea " + (this.props.isUpvoted ? 'Idea--upvoted' : '')}
-        onClick={this.props.onClick}>
-        <p className="Idea__summary">{this.props.summary} (by {this.props.userName})</p>
-        <div className="Idea__upvotes">{this.props.upvotes}</div>
-      </li>
-    );
-  }
 }
 
 export default Idea;
