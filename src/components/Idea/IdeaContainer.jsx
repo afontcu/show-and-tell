4d1ffa2fd2b993be6a5a31a0f5e9116e849ba966
Adrià Fontcuberta <adria@calidae.com>
2017-06-05 17:36:47 +0200

    use state to count upvotes

--- src/components/Idea/IdeaContainer.jsx
+++ src/components/Idea/IdeaContainer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Idea from './Idea';
 import { graphql } from 'react-apollo';
-import { UpvoteIdeaQuery } from '../../queries/UpvoteIdeaQuery';
+import { UpvoteIdeaMutation } from '../../queries/UpvoteIdeaMutation';
 // const nl2br = require('react-nl2br');
 
 const LOCAL_STORAGE_PREFIX = 'voted-item-';
@@ -12,7 +12,6 @@ class IdeaContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      upvotes: this.props.idea.upvotes,
       isUpvoted: this.hasBeenUpvoted()
     };
 
@@ -25,24 +24,19 @@ class IdeaContainer extends React.Component {
   }
 
   handleClick() {
-
     if (!this.hasBeenUpvoted()) {
       this.props.mutate({
         variables: { IdeaID: this.props.idea.id }
       })
-      .then(({ data }) => {
-          console.log('got data', data);
-
+      .then(() => {
           window.localStorage.setItem(
             LOCAL_STORAGE_PREFIX + this.props.idea.id,
             LOCAL_STORAGE_UPVOTED_VALUE
           );
 
           this.setState({
-            upvotes: data.upvoteIdea.idea.upvotes,
             isUpvoted: true
           });
-
       })
       .catch((error) => {
           console.log('there was an error sending the query', error);
@@ -53,11 +47,10 @@ class IdeaContainer extends React.Component {
   }
 
   render() {
-    
     return (
         <Idea
             isUpvoted={this.state.isUpvoted}
-            upvotes={this.state.upvotes}
+            upvotes={this.props.idea.upvotes}
             summary={this.props.idea.summary}
             userName={this.props.idea.userName}
             onClick={this.handleClick}
@@ -66,4 +59,4 @@ class IdeaContainer extends React.Component {
   }
 }
 
-export default graphql(UpvoteIdeaQuery)(IdeaContainer);
+export default graphql(UpvoteIdeaMutation)(IdeaContainer);
