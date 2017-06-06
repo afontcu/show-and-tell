9ffc312a5f2fb88db7f4faaf1fea5f0d4107a193
Adrià Fontcuberta <adria@calidae.com>
2017-06-06 08:57:28 +0200

    catalanize literals

--- src/components/SubmitIdea/SubmitIdea.jsx
+++ src/components/SubmitIdea/SubmitIdea.jsx
@@ -3,39 +3,37 @@ import './SubmitIdea.css';
 
 function SubmitIdea(props) {
     return (
-    <div>
-        <br /><br />
-        <h2>Submit a new Idea</h2>
-        <form className="SubmitIdea" action="#" onSubmit={props.onSubmit}>
-            <input 
-                className="SubmitIdea__input"
-                required
-                type="text" 
-                value={props.userName}
-                onChange={props.handleInputChange}
-                name="userName"
-                placeholder="Who are you?" />
-            <input
-                className="SubmitIdea__input"
-                required
-                type="text" 
-                value={props.summary}
-                onChange={props.handleInputChange}
-                name="summary"
-                placeholder="What is your idea?" />
-            <textarea
-                className="SubmitIdea__input"
-                required
-                value={props.explanation}
-                onChange={props.handleInputChange}
-                name="explanation"
-                placeholder="Describe it in a few words" />
-            <input 
-                className="SubmitIdea__button"
-                type="submit"
-                value="Submit Idea" />
-        </form>
-    </div>
+    <form className="SubmitIdea" action="#" onSubmit={props.onSubmit}>
+        <h2 className="SubmitIdea__title">Proposa un nou Show&Tell</h2>
+        <input 
+            className="SubmitIdea__input"
+            required
+            type="text" 
+            value={props.userName}
+            onChange={props.handleInputChange}
+            name="userName"
+            placeholder="Qui ets?" />
+        <input
+            className="SubmitIdea__input"
+            required
+            type="text" 
+            value={props.summary}
+            onChange={props.handleInputChange}
+            name="summary"
+            placeholder="Quina idea tens?" />
+        <textarea
+            className="SubmitIdea__input"
+            required
+            value={props.explanation}
+            onChange={props.handleInputChange}
+            name="explanation"
+            rows="5"
+            placeholder="Descriu-la en poques paraules" />
+        <input 
+            className="SubmitIdea__button"
+            type="submit"
+            value="Envia la idea!" />
+    </form>
     );
 }
 
